21502fd31a1ed1cf0458ccc820f6a94dbded0c44
Iliyan <ilianbb4@gmail.com>
2021-04-17 23:44:08 +0300

    Fixed acording to final example

--- IlkoOS.h
+++ IlkoOS.h
@@ -1,10 +1,17 @@
 #include <ucontext.h>
 #include <string>
 #include <math.h>
-// #include <thread_structures.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <sys/time.h>
+#include <signal.h>
+#include <unistd.h>
 
 #define FIBER_STACK 1024 * 64
+#define INTERVAL 1
 typedef struct tcb_t Task;
+static ucontext_t a;
 
 namespace IlkoOS
 {
@@ -12,13 +19,13 @@ namespace IlkoOS
     {
         struct Task
         {
-            ucontext_t context;
+            ucontext_t *context;
             void (*func)();
             Task *next;
 
             Task()
             {
-                this->context = ucontext_t();
+                this->context = NULL;
                 this->func = NULL;
                 this->next = NULL;
             }
@@ -27,68 +34,96 @@ namespace IlkoOS
             {
                 ucontext_t *context = new ucontext_t();
                 //Sets the context of the new task;
-                getcontext(context);
-                context->uc_link = &next->context;
+                if (getcontext(context) == -1)
+                {
+                    printf("Getcontext failed.\n");
+                    exit(1);
+                };
+                context->uc_link = &a;
                 context->uc_stack.ss_sp = malloc(FIBER_STACK);
                 context->uc_stack.ss_size = FIBER_STACK;
                 context->uc_stack.ss_flags = 0;
                 makecontext(context, func, 0);
 
-                this->context = *context;
+                this->context = context;
                 this->func = func;
                 this->next = next;
             }
 
-        } * running_task, *task_queue;
+        } * task_queue;
 
-        void push(void (*start_routine)())
+        void push(void (*func)())
         {
-            if (task_queue != NULL)
-            {
-                // //Sets the context of the new task;
-                // getcontext(&context);
-                // // context.uc_link = &task_queue.context;
-                // context.uc_stack.ss_sp = malloc(FIBER_STACK);
-                // context.uc_stack.ss_size = FIBER_STACK;
-                // context.uc_stack.ss_flags = 0;
-                // makecontext(&context, start_routine, 0);
-
-                //Creates new Task and sets to head;
-                task_queue = new Task(start_routine, task_queue);
-            }
-            else
-            {
-                // //Sets the context of the new task;
-                // getcontext(&context);
-                // context.uc_link = NULL;
-                // context.uc_stack.ss_sp = malloc(FIBER_STACK);
-                // context.uc_stack.ss_size = FIBER_STACK;
-                // context.uc_stack.ss_flags = 0;
-                // makecontext(&context, start_routine, 0);
-
-                //Sets new task to head;
-                task_queue = new Task(start_routine, NULL);
-            }
+            task_queue = new Task(func, task_queue);
         }
     }
+
+    void execute_task(int signum)
+    {
+
+        printf("Signal recieved!\nExecuting function:\n");
+
+        if (swapcontext(&a, task_queue->context) == -1)
+        {
+            printf("Swapcontext failed.\n");
+            exit(1);
+        };
+
+        if (task_queue->next == NULL || task_queue->next->context == NULL)
+        {
+            printf("No next function to execute.\n");
+            exit(1);
+        }
+
+        task_queue = task_queue->next;
+    };
 
     void initlibrary()
     {
         //Sets end to beggining creating a loop;
         task_queue->next->next->next->next = task_queue;
 
-        // //Execute a function and change to next context;
-        // while (true)
+        // Task *last_task = task_queue;
+        // while (last_task->next != NULL)
         // {
-        task_queue = task_queue->next;
-        running_task = task_queue;
-        setcontext(&running_task->context);
-        // };
+        //     last_task = last_task->next;
+        // }
+        // last_task->next = task_queue;
+
+        printf("Creating new timer.\n\n");
+
+        struct itimerval it_val;
+        it_val.it_value.tv_sec = INTERVAL;
+        it_val.it_value.tv_usec = 0;
+        it_val.it_interval.tv_sec = INTERVAL;
+        it_val.it_interval.tv_usec = 0;
+        if (setitimer(ITIMER_REAL, &it_val, NULL) == -1)
+        {
+            printf("Setitimer failed.\n");
+            exit(1);
+        };
+
+        struct sigaction action;
+        action.sa_handler = execute_task;
+        if (sigemptyset(&action.sa_mask) == -1)
+        {
+            printf("Sigemptyset failed.\n");
+            exit(1);
+        };
+        action.sa_flags = 0;
+        if (sigaction(SIGALRM, &action, NULL) == -1)
+        {
+            printf("Sigaction failed.\n");
+            exit(1);
+        };
+
+        while (true)
+            ;
     };
 
-    int create_task(void (*start_routine)())
+    int create_task(void (*func)())
     {
-        push(start_routine);
+        push(func);
 
         return 0;
     };
--- Test.cpp
+++ Test.cpp
@@ -8,8 +8,6 @@
 #include <signal.h>
 #include <unistd.h>
 
-static ucontext_t a;
-
 void f1()
 {
     puts("-");
@@ -34,40 +32,6 @@ void f4()
     setcontext(&a);
 }
 
-#define INTERVAL 1
-
-void TimerStop(int signum)
-{
-
-    printf("Signal recieved!\nExecuting function:\n");
-
-    //Shows next char;
-    IlkoOS::initlibrary();
-}
-
-void TimerSet(int interval)
-{
-    printf("\n\n\n");
-    printf("Creating new signal.\n\n");
-    struct itimerval it_val;
-
-    it_val.it_value.tv_sec = interval;
-    it_val.it_value.tv_usec = 0;
-    it_val.it_interval.tv_sec = 0;
-    it_val.it_interval.tv_usec = 0;
-
-    if (signal(SIGALRM, TimerStop) == SIG_ERR)
-    {
-        perror("Unable to catch SIGALRM");
-        exit(1);
-    }
-    if (setitimer(ITIMER_REAL, &it_val, NULL) == -1)
-    {
-        perror("error calling setitimer()");
-        exit(1);
-    }
-}
-
 using namespace IlkoOS;
 
 int main()
@@ -78,16 +42,7 @@ int main()
     IlkoOS::create_task(f3);
     IlkoOS::create_task(f4);
 
-    //Main loop;
-    while (1)
-    {
-        //Create main context;
-        getcontext(&a);
+    IlkoOS::initlibrary();
 
-        sleep(2);
-
-        //Creates timer;
-        TimerSet(INTERVAL);
-    }
     return 0;
 }
